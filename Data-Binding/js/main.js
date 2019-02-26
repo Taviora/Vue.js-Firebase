a6816d75c2ce5a054003d2a89410ccf166c1754d
Gustavo <gus20solji@gmail.com>
2019-02-25 18:08:18 -0600

    Data binding introduction.

---
+++ Data-Binding/js/main.js
@@ -0,0 +1,15 @@
+//Vainilla javascript
+
+// const h1 = document.querySelector('h1');
+// const input = document.querySelector('input');
+// input.addEventListener('keyup', () =>  h1.innerHTML = input.value);
+
+//VUE.js
+
+new Vue({
+
+    el: 'main',
+    data:{
+        message: "Hola mundo!",
+    }
+});
